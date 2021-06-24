12c00817ad083b340d6c7c5ab98d5cf1c8964f99
Jyoti Vankala <jyotivankala123@gmail.com>
2021-06-24 19:37:55 +0530

    upload

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -14,14 +14,14 @@ const adminController = require('../controllers/Admin/AdminController');
 /* ############################################ Routes  ############################################ */
 
 router.post('/login', validateRequest.validate(adminValidationSchema.loginSchema, 'body'), adminController.adminLogin); // System Login Route
-router.post('/country-add', authenticationMiddleware.authenticateAdminRequestAPI, validateRequest.validate(adminValidationSchema.addCountrySchema, 'body'), adminController.addCountry); // Add Country
-router.get('/country-list', authenticationMiddleware.authenticateAdminRequestAPI, validateRequest.validate(adminValidationSchema.listCountrySchema, 'query'), adminController.listCountry); // List Country
-router.put('/country-status-change/:id', authenticationMiddleware.authenticateAdminRequestAPI, adminController.statusChangeCountry); // Country Status Change
-router.post('/genre-add', authenticationMiddleware.authenticateAdminRequestAPI, validateRequest.validate(adminValidationSchema.addGenreSchema, 'body'), adminController.addGenre); // Add Genre
-router.get('/genre-list', authenticationMiddleware.authenticateAdminRequestAPI, validateRequest.validate(adminValidationSchema.listCountrySchema, 'query'), adminController.listGenre); // List Genre
-router.delete('/delete-genre/:id', authenticationMiddleware.authenticateAdminRequestAPI, adminController.deleteGenre); // Delete Genre
+router.post('/country-add', authenticationMiddleware.authenticateArtistRequestAPI, validateRequest.validate(adminValidationSchema.addCountrySchema, 'body'), adminController.addCountry); // Add Country
+router.get('/country-list', authenticationMiddleware.authenticateArtistRequestAPI, validateRequest.validate(adminValidationSchema.listCountrySchema, 'query'), adminController.listCountry); // List Country
+router.put('/country-status-change/:id', authenticationMiddleware.authenticateArtistRequestAPI, adminController.statusChangeCountry); // Country Status Change
+router.post('/genre-add', authenticationMiddleware.authenticateArtistRequestAPI, validateRequest.validate(adminValidationSchema.addGenreSchema, 'body'), adminController.addGenre); // Add Genre
+router.get('/genre-list', authenticationMiddleware.authenticateArtistRequestAPI, validateRequest.validate(adminValidationSchema.listCountrySchema, 'query'), adminController.listGenre); // List Genre
+router.delete('/delete-genre/:id', authenticationMiddleware.authenticateArtistRequestAPI, adminController.deleteGenre); // Delete Genre
 router.get('/artist-list', authenticationMiddleware.authenticateArtistRequestAPI, validateRequest.validate(adminValidationSchema.listCountrySchema, 'query'), adminController.listArtists); // List Artists
-router.get('/artist-details/:id', authenticationMiddleware.authenticateAdminRequestAPI, adminController.artistDetails); // Artists Details
+router.get('/artist-details/:id', authenticationMiddleware.authenticateArtistRequestAPI, adminController.artistDetails); // Artists Details
 router.put('/artist-verify/:id', authenticationMiddleware.authenticateAdminRequestAPI, adminController.verifyArtist); // Artists verify
 router.put('/accept-artist/:id', authenticationMiddleware.authenticateAdminRequestAPI, adminController.acceptArtist); // Accept verify
 router.put('/decline-artist/:id', authenticationMiddleware.authenticateAdminRequestAPI, validateRequest.validate(adminValidationSchema.declineArtistSchema, 'body'),adminController.declineArtist); // Decline Artist
